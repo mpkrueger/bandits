c25b3637083a8d2ec6d36ee13601ae4d3bfd4abb
Sushmita <ssubrama@gmail.com>
2016-09-14 14:04:58 -0700

    checking in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914202011) do
+ActiveRecord::Schema.define(version: 20160914204044) do
 
   create_table "colleges", force: :cascade do |t|
     t.integer  "goal_id"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20160914202011) do
     t.integer  "recipient_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "first_name"
     t.index ["family_id"], name: "index_invites_on_family_id"
   end
 
