40da05931e75754f5b1998130ec09198d95516b7
Sushmita <ssubrama@gmail.com>
2016-09-15 13:37:07 -0700

    redid dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,31 @@ class DashboardController < ApplicationController
 	  	@family = @user.family
 	  	@family_members = @user.family.users.count
 	  	@family_goals = @user.family.goals
-	  end
-  end
+
+	  	@family_goals.each do |goal|
+		  	if(goal.trips.present?)
+		  		@trip = goal.trips.first
+		  		@tripcost =  @trip.flight_cost + (@trip.hotel_cost * @trip.duration) + (@trip.fun_cost * @trip.duration)
+		  	end
+	  	end
+	end
+
+ 	# image search
+    @image_options = {}
+    @image_options[:searchType] = "image"
+    @image_options[:imgSize] = "xlarge"
+    @images = []
+    @results = GoogleCustomSearchApi.search(@trip.place, @image_options)
+
+    # filter to keep images where height < width
+    @results.items.each do |result|
+		height = result["image"]["height"]
+		width = result["image"]["width"]
+		if height < width
+		@images.push result
+		end
+	end
+
+
+end
 end
