179b7deecdeea667dbc1214f0fbed5bc78fba099
Matt Krueger <mpkrueger@gmail.com>
2016-09-15 15:01:26 -0700

    fixing bug with date construction on mobile devices

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,10 +6,14 @@ class Trip < ApplicationRecord
   end
 
   def trip_date=(my_date)
-  	split = my_date.split(', ', 2)
-  	month = split.first
-  	year = split.last
-  	numeric_month = Date::MONTHNAMES.index(month)
-  	self.date = "#{year}-#{numeric_month}-01"
+  	if browser.device.mobile?
+      self.date = my_date + "-01"
+    else
+      split = my_date.split(', ', 2)
+    	month = split.first
+    	year = split.last
+    	numeric_month = Date::MONTHNAMES.index(month)
+    	self.date = "#{year}-#{numeric_month}-01"
+    end
   end
 end
