cde35cb942300616c89076fe1bf8f773fd78e417
Matt Krueger <mpkrueger@gmail.com>
2016-09-15 15:11:25 -0700

    trying regex to convert the date depending on format in the trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,14 +7,14 @@ class Trip < ApplicationRecord
   end
 
   def trip_date=(my_date)
-  	if browser.device.mobile?
-      self.date = my_date + "-01"
-    else
+  	if my_date =~ /a-zA-Z/
       split = my_date.split(', ', 2)
-    	month = split.first
-    	year = split.last
-    	numeric_month = Date::MONTHNAMES.index(month)
-    	self.date = "#{year}-#{numeric_month}-01"
+      month = split.first
+      year = split.last
+      numeric_month = Date::MONTHNAMES.index(month)
+      self.date = "#{year}-#{numeric_month}-01"
+    else
+      self.date = my_date + "-01"
     end
   end
 end
