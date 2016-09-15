4c9a9ebb42055b2b490454af7b27bd1f50a07a88
Matt Krueger <mpkrueger@gmail.com>
2016-09-15 13:52:42 -0700

    tucking image search in trip.present loop in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,24 +10,24 @@ class DashboardController < ApplicationController
 		  	if(goal.trips.present?)
 		  		@trip = goal.trips.first
 		  		@tripcost =  @trip.flight_cost + (@trip.hotel_cost * @trip.duration) + (@trip.fun_cost * @trip.duration)
+
+		  		# image search
+			    @image_options = {}
+			    @image_options[:searchType] = "image"
+			    @image_options[:imgSize] = "xlarge"
+			    @images = []
+			    @results = GoogleCustomSearchApi.search(@trip.place, @image_options)
+
+			    # filter to keep images where height < width
+			    @results.items.each do |result|
+						height = result["image"]["height"]
+						width = result["image"]["width"]
+						if height < width
+							@images.push result
+						end
+		  		end
 		  	end
 	  	end
-	end
-
- 	# image search
-    @image_options = {}
-    @image_options[:searchType] = "image"
-    @image_options[:imgSize] = "xlarge"
-    @images = []
-    @results = GoogleCustomSearchApi.search(@trip.place, @image_options)
-
-    # filter to keep images where height < width
-    @results.items.each do |result|
-		height = result["image"]["height"]
-		width = result["image"]["width"]
-		if height < width
-		@images.push result
-		end
 	end
 
 
