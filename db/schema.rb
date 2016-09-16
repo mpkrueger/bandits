996d86f7e146d2c9a45ea5c3480aaf79f0ed313c
Sushmita <ssubrama@gmail.com>
2016-09-16 14:22:23 -0700

    merging stuff

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20160916192258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "colleges", force: :cascade do |t|
     t.integer  "goal_id"
